02d09c6161d48269eb0b94574ba9ab8fe4fb7279
steven28zhang <stephenxianweizhang@gmail.com>
2014-06-03 14:32:47 +0800

    indexProV0.1
    copy the existing function;
    remove personal data

---
+++ indexgae/src/com/sxwz/index/gae/client/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package com.sxwz.index.gae.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+    void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
+}
